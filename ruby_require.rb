6d64d5b06a9b8cf456bffbd2bc4393a3a254b1ea
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2018-04-27 01:41:12 +0700

    -> require on ruby

---
+++ ruby_require.rb
@@ -0,0 +1,10 @@
+# using load
+# load '/home/jeffryangtoni/Sources/GitHub/ruby-projects/ruby-learning/oop1.rb'
+# require '/home/jeffryangtoni/Sources/GitHub/ruby-projects/ruby-learning/oop1.rb'
+require_relative 'oop1.rb'
+
+test = Rectangle.new(3, 5)
+puts test.countArea
+
+t1 = Time.now
+puts t1
